0f1da4ac7647a7b25ab9fd762265dc7ae148907b
YS1306 <71446598+YS1306@users.noreply.github.com>
2024-12-27 16:08:45 +0530

    Time: 23 ms (5.39%) | Memory: 14.4 MB (5.87%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.cpp
+++ 13-roman-to-integer/roman-to-integer.cpp
@@ -1,47 +1,28 @@
 class Solution {
 public:
     int romanToInt(string s) {
-        int i=0;
+        int n = s.size();
+        int i=n-2;
         unordered_map<char, int> priority = { {'I',1}, {'V', 2}, {'X', 3}, {'L', 4}, {'C', 5}, {'D', 6}, {'M',7} };
         unordered_map<char, int> values = { {'I',1}, {'V', 5}, {'X', 10}, {'L', 50}, {'C', 100}, {'D', 500}, {'M',1000} };
         vector<char> stack;
         int res=0;
         int top=-1;
-        while(s[i] != '\0'){
-            if(top == -1){  stack.push_back(s[i]);  top++;}
-            else{
-                if(priority[s[i]] > priority[stack[top]]){
-                    stack.push_back(s[i]); top++;
-                }
-                else if(priority[s[i]] == priority[stack[top]]){
-                    res += 2*values[s[i]];
-                    stack.pop_back();
-                    top--;                }
-                else{
-                    int temp = values[stack[top]];
-                    stack.pop_back();
-                    top--;
-                    while(top >= 0){
-                        temp -= values[stack[top]];
-                        stack.pop_back();
-                        top--;
-                    }
-                    res += temp;
-                    stack.push_back(s[i]);
-                    top++;
-                }
+        int temp = values[s[n-1]];
+        char prev = s[n-1];
+        while(i >= 0){
+            char curr = s[i];
+            if(priority[curr] < priority[prev]){
+                temp -= values[curr];
             }
-            i++;
+            else if(priority[curr] >= priority[prev]){
+                res += temp;
+                temp = values[curr];
+                prev = curr;
+            }
+            i--;
         } 
-        if(top == -1)   return res;
-        int temp = values[stack[top]];  top--;     
-        while(top >= 0){
-            temp -= values[stack[top]];
-            stack.pop_back();
-            top--;
-        }
-        res += temp;
-        return res;
+        return res+temp;    
     }
 
 
