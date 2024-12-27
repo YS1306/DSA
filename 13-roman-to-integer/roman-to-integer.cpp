e1bce4f2995396042372e744393d828a19de9b54
YS1306 <71446598+YS1306@users.noreply.github.com>
2024-12-27 16:57:34 +0530

    Time: 5 ms (65.14%) | Memory: 13.4 MB (29.48%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.cpp
+++ 13-roman-to-integer/roman-to-integer.cpp
@@ -3,19 +3,16 @@ public:
     int romanToInt(string s) {
         int n = s.size();
         int i=n-2;
-        unordered_map<char, int> priority = { {'I',1}, {'V', 2}, {'X', 3}, {'L', 4}, {'C', 5}, {'D', 6}, {'M',7} };
         unordered_map<char, int> values = { {'I',1}, {'V', 5}, {'X', 10}, {'L', 50}, {'C', 100}, {'D', 500}, {'M',1000} };
-        vector<char> stack;
         int res=0;
-        int top=-1;
         int temp = values[s[n-1]];
         char prev = s[n-1];
         while(i >= 0){
             char curr = s[i];
-            if(priority[curr] < priority[prev]){
+            if(values[curr] < values[prev]){
                 temp -= values[curr];
             }
-            else if(priority[curr] >= priority[prev]){
+            else if(values[curr] >= values[prev]){
                 res += temp;
                 temp = values[curr];
                 prev = curr;
