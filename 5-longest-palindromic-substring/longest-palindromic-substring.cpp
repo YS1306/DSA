b33f2d06151b6cca1e4769097baf178d8b06c8a1
YS1306 <71446598+YS1306@users.noreply.github.com>
2024-12-28 21:33:51 +0530

    Time: 947 ms (8.46%) | Memory: 471.4 MB (5.04%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -1,46 +1,8 @@
 class Solution {
 public:
-    string odd_pali(string s, int i){
-        int n = s.length();
-        string temp = "";
-        temp = temp+string(1,s[i]);
-        int j = 1;
-        while(i-j >= 0 && i+j < n && s[i-j] == s[i+j]){
-            temp = string(1,s[i-j])+temp+string(1,s[i+j]);
-            j++;
-        }
-        return temp;
-    }
-
-    string even_pali(string s, int i){
-        int n = s.length();
-        bool left;
-        string temp = "";
-        if(i-1 >=0 && s[i-1] == s[i]){  left = true;   temp += string(1,s[i-1])+string(1,s[i]);     }
-        else if(i+1 <n && s[i] == s[i+1]){ left = false;  temp += string(1,s[i])+string(1,s[i+1]);     }
-        else return temp;
-        if(left){
-            int j = 1;
-
-            while(i-1-j >= 0 && i+j < n && s[i-1-j] == s[i+j]){
-                temp = string(1,s[i-1-j]) + temp + string(1,s[i+j]);
-                j++;
-            }
-            return temp;
-        }
-        else{
-            int j = 1;
-            while(i-j >= 0 && i+1+j < n && s[i-j] == s[i+1+j]){
-
-                temp = string(1,s[i-j]) + temp + string(1,s[i+1+j]);
-                j++;
-            }
-            return temp;
-        }
-    }
-
     string longestPalindrome(string s) {
         int n = s.size();
+        if(n <= 1)   return s; 
         string res = "";
         string temp = "";
         for(int i=1; i < n; i++){
@@ -55,7 +17,6 @@ public:
             if(temp.size() > res.size())    res = temp;
             temp = "";
 
-            // string even = even_pali(s, i);
             j = 0;
             while(i-1-j >= 0 && i+j < n && s[i-1-j] == s[i+j]){
                 temp = s.substr(i-1-j,2*(j+1));
@@ -64,7 +25,7 @@ public:
             if(temp.size() > res.size())    res = temp;
             temp = "";
         }        
-        if(res =="")    return string(1,s[0]);
+        // if(res =="")    return string(1,s[0]);
         return res;
     }
 };
