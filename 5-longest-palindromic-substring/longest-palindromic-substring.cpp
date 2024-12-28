029d78dd0b455d01825751c3eeb69de87f20ac4c
YS1306 <71446598+YS1306@users.noreply.github.com>
2024-12-28 23:13:37 +0530

    Time: 990 ms (8.11%) | Memory: 471.6 MB (5.04%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -1,48 +1,31 @@
 class Solution {
 public:
-    bool ispal(string temp){
-        int n = temp.size();
-        for(int i=0; i <= n/2; i++){
-            if(temp[i] != temp[n-i-1])  return false;
-        }
-        return true;
-    }
-
-    string check(string s, string rev){
-        int i = 0;
-        int n = s.size();
-        string res = "";
-        while(i<n && n-i > res.size()/2){
-            int j = 0;
-            while(j < n-i){
-                if(s[i] == rev[j]){
-                    int k = 0;
-                    int next = 0;
-                    string temp = "";
-                    int flag = true;
-                    while(i+k < n && j+k < n && s[i+k] == rev[j+k]){
-                        temp.push_back(s[i+k]);
-                        k++;
-                        if(rev[j+k] == s[i]){    next = j+k;  }
-                    }
-                    if(temp.size() > res.size() and ispal(temp))    res = temp;
-                    if(next)    j=next;
-                    else j = j+k;
-                }
-                else j++;
-            }
-            i++;
-        }    
-        return res;    
-    }
-
     string longestPalindrome(string s) {
-        
-        string rev = s;
-        reverse(rev.begin(), rev.end());
-        string front = check(s, rev);
-        string back = check(rev, s);
+        int n = s.size();
+        if(n <= 1)   return s; 
+        string res = "";
+        string temp = "";
+        for(int i=0; i < n; i++){
+            // string odd = odd_pali(s, i);
 
-        return max(front, back);
+            temp = s.substr(i,1);
+            int j = 1;
+            while(i-j >= 0 && i+j < n && s[i-j] == s[i+j]){
+                temp = s.substr(i-j, 2*j+1);
+                j++;
+            }
+            if(temp.size() > res.size())    res = temp;
+            temp = "";
+
+            j = 0;
+            while(i-1-j >= 0 && i+j < n && s[i-1-j] == s[i+j]){
+                temp = s.substr(i-1-j,2*(j+1));
+                j++;
+            }
+            if(temp.size() > res.size())    res = temp;
+            temp = "";
+        }        
+        // if(res =="")    return string(1,s[0]);
+        return res;
     }
 };
