2abe879cf1c7d33201f9e6a172c9e0ab911a9f8a
YS1306 <71446598+YS1306@users.noreply.github.com>
2024-12-28 18:10:16 +0530

    Time: 477 ms (15.12%) | Memory: 110.8 MB (28.65%) - LeetSync

---
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    bool ispal(string temp){
+        int n = temp.size();
+        for(int i=0; i <= n/2; i++){
+            if(temp[i] != temp[n-i-1])  return false;
+        }
+        return true;
+    }
+
+    string check(string s, string rev){
+        int i = 0;
+        int n = s.size();
+        string res = "";
+        while(i<n){
+            int j = 0;
+            while(j < n-i){
+                if(s[i] == rev[j]){
+                    int k = 0;
+                    int next = 0;
+                    string temp = "";
+                    int flag = true;
+                    while(i+k < n && j+k < n && s[i+k] == rev[j+k]){
+                        temp.push_back(s[i+k]);
+                        k++;
+                        if(rev[j+k] == s[i]){    next = j+k;  }
+                    }
+                    if(temp.size() > res.size() and ispal(temp))    res = temp;
+                    if(next)    j=next;
+                    else j = j+k;
+                }
+                else j++;
+            }
+            i++;
+        }    
+        return res;    
+    }
+
+    string longestPalindrome(string s) {
+        
+        string rev = s;
+        reverse(rev.begin(), rev.end());
+        string front = check(s, rev);
+        string back = check(rev, s);
+
+        return max(front, back);
+    }
+};
