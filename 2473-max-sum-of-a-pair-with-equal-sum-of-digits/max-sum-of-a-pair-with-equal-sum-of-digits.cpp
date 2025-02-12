3b7a2543df8368b88c482ed6ebcfbb3b532662af
YS1306 <71446598+YS1306@users.noreply.github.com>
2025-02-13 00:02:25 +0530

    Time: 35 ms (42.73%) | Memory: 63.9 MB (61.17%) - LeetSync

--- 2473-max-sum-of-a-pair-with-equal-sum-of-digits/max-sum-of-a-pair-with-equal-sum-of-digits.cpp
+++ 2473-max-sum-of-a-pair-with-equal-sum-of-digits/max-sum-of-a-pair-with-equal-sum-of-digits.cpp
@@ -12,12 +12,8 @@ public:
                 num = num/10;
             }
             if(sums.find(sum) != sums.end()){
-                if(sums[sum]+nums[i] > res){
-                    res = sums[sum]+nums[i];
-                }
-                if(nums[i] > sums[sum]){
-                    sums[sum] = nums[i];
-                }
+                res = max(res,sums[sum]+nums[i]);
+                sums[sum] = max(sums[sum], nums[i]);
             }
             else{
                 sums[sum] = nums[i];
