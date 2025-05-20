4c1811355d1a79056220c4504f5d797783815c21
YS1306 <71446598+YS1306@users.noreply.github.com>
2025-05-20 23:30:31 +0530

    Time: 1 ms (53.95%) | Memory: 18 MB (8.72%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.py
+++ 8-string-to-integer-atoi/string-to-integer-atoi.py
@@ -3,6 +3,7 @@ class Solution:
     def Atoi(self, s:str, sign:int):
         if(len(s) == 0):
             return 0
+        # if(len(s) == 1 and )
         
         if(s[0] in [' ', '+', '-']):
             return 0
@@ -11,7 +12,7 @@ class Solution:
             res = self.Atoi(s[1:], sign)
             self.digits += 1
             if(sign == -1):
-                return min(10**(self.digits-1)*int(s[0])+res,2**(31))
+                return min(10**(self.digits-1)*int(s[0])+res, 2**(31))
             else:
                 return min(10**(self.digits-1)*int(s[0])+res, 2**(31)-1)
         return 0
@@ -20,9 +21,7 @@ class Solution:
         if(len(s) == 0):
             return 0
         while(s[0] == ' '):
-            s = s[1:]
-            if(s == ''):
-                return 0
+            return self.myAtoi(s[1:])
         if(s[0] == '-'):
             sign = -1
             return -1*self.Atoi(s[1:], sign)
