405927e174febae369b03fce3d381d54fa8c481c
YS1306 <71446598+YS1306@users.noreply.github.com>
2025-06-10 23:42:07 +0530

    Time: 22 ms (13.60%) | Memory: 18.1 MB (19.28%) - LeetSync

--- 39-combination-sum/combination-sum.py
+++ 39-combination-sum/combination-sum.py
@@ -2,7 +2,7 @@ class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         res = []
         def check(nums, curr_sum, target):
-            # print(curr_sum)
+
             if sum(curr_sum) > target:
                 return 
             if sum(curr_sum) == target:
