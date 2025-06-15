d1fe964f6c034dd137f7cfe70c2703799c727d2d
YS1306 <71446598+YS1306@users.noreply.github.com>
2025-06-15 19:50:42 +0530

    Time: 11 ms (6.82%) | Memory: 18.2 MB (15.11%) - LeetSync

--- 90-subsets-ii/subsets-ii.py
+++ 90-subsets-ii/subsets-ii.py
@@ -2,6 +2,7 @@ class Solution:
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
         res = []
         def check(nums, curr_list):
+            print(curr_list)
             if len(nums) == 0:
                 if curr_list not in res:
                     res.append(curr_list)
@@ -9,10 +10,15 @@ class Solution:
             if curr_list not in res:
                 res.append(curr_list)
 
-        
-            check(nums[1:], curr_list+[nums[0]])
-            check(nums[1:], curr_list)
 
+            check(nums[1:], curr_list+[nums[0]])
+            while(len(nums)>1 and  nums[0] == nums[1]):
+                nums = nums[1:]
+            # if len(nums)>1 and  nums[0] == nums[1]:
+            #     check(nums[2:], curr_list)
+            # else:
+            check(nums[1:], curr_list)
+        
         nums = sorted(nums)
         check(nums, [])
         return res
