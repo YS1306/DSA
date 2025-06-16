cf8eb6450bc84085c507e398a70a2ead687acffa
YS1306 <71446598+YS1306@users.noreply.github.com>
2025-06-16 23:10:20 +0530

    Time: 3 ms (23.95%) | Memory: 17.8 MB (59.01%) - LeetSync

--- 216-combination-sum-iii/combination-sum-iii.py
+++ 216-combination-sum-iii/combination-sum-iii.py
@@ -1,20 +1,20 @@
 class Solution:
     def combinationSum3(self, k: int, n: int) -> List[List[int]]:
         res = []
-        def check(nums, curr_list, curr_sum):
+        def check(nums, curr_list, curr_sum, target):
         
             if len(curr_list) == k:
-                if curr_sum == n and curr_list not in res:
+                if target == 0:
                     res.append(curr_list)
                 return
             
             if len(nums) == 0:
                 return 
             
-            check(nums[1:], curr_list+[nums[0]], curr_sum+nums[0])
-            check(nums[1:], curr_list, curr_sum)
+            check(nums[1:], curr_list+[nums[0]], curr_sum+nums[0], target-nums[0])
+            check(nums[1:], curr_list, curr_sum, target)
 
 
         nums = [i for i in range(1,min(10,n))]
-        check(nums, [], 0)
+        check(nums, [], 0, n)
         return res
