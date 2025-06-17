eea3fb108a758514141edc8b1b404ec253509e7c
YS1306 <71446598+YS1306@users.noreply.github.com>
2025-06-17 20:24:24 +0530

    Time: 0 ms (100.00%) | Memory: 18 MB (23.14%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
@@ -5,7 +5,7 @@ class Solution:
         res = []
         letters = {2:["a","b","c"], 3:["d","e","f"], 4:["g","h","i"], 5:["j","k","l"], 6:["m","n","o"], 7:["p","q","r","s"], 8:["t","u","v"], 9:["w","x","y","z"]}
 
-        def check(rem, curr):
+        def check(rem, curr, letters):
             
             if len(rem) == 0:
                 if curr not in res:
@@ -13,7 +13,7 @@ class Solution:
                 return
             
             for let in letters[int(rem[0])]:
-                check(rem[1:], curr+let)
+                check(rem[1:], curr+let, letters)
             
-        check(digits, "")
+        check(digits, "", letters)
         return res
