f2ac06323c8ff66cca1f37bcd1d23303b6c91354
YS1306 <71446598+YS1306@users.noreply.github.com>
2025-06-17 20:23:07 +0530

    Time: 1 ms (7.39%) | Memory: 18 MB (23.14%) - LeetSync

---
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
@@ -0,0 +1,19 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        if len(digits) == 0:
+            return []
+        res = []
+        letters = {2:["a","b","c"], 3:["d","e","f"], 4:["g","h","i"], 5:["j","k","l"], 6:["m","n","o"], 7:["p","q","r","s"], 8:["t","u","v"], 9:["w","x","y","z"]}
+
+        def check(rem, curr):
+            
+            if len(rem) == 0:
+                if curr not in res:
+                    res.append(curr)
+                return
+            
+            for let in letters[int(rem[0])]:
+                check(rem[1:], curr+let)
+            
+        check(digits, "")
+        return res
