a9f4bb72fd945a91be576613f36cc115af34eed2
YS1306 <71446598+YS1306@users.noreply.github.com>
2025-06-17 23:40:35 +0530

    Time: 96 ms (7.85%) | Memory: 34.6 MB (45.22%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.py
+++ 131-palindrome-partitioning/palindrome-partitioning.py
@@ -1,25 +1,18 @@
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
-        #go upto full 
-
-        #check from last
-        # call check, if check returns True upto last -> append all to res
-
-        # Take 1 element in set and call for next, then 2 in set, then 
-
-        # if curr[-1] not palindrome    return
-        # if idx = -1: if palindrome append curr
         res = []
 
-        def isPali(st):
-            n = len(st)
-            for i in range(n//2):
-                if st[i] != st[n-i-1]:
-                    return False
-
-            return True
+        
 
         def check(nums, curr_list):
+            def isPali(st):
+                n = len(st)
+                for i in range(n//2):
+                    if st[i] != st[n-i-1]:
+                        return False
+
+                return True
+
             if len(curr_list) > 0 and not isPali(curr_list[-1]):
                 return
             if len(nums) == 0:
