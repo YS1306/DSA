a9dd1c5ee8a0a7aeaf23871d34036a8570938ae9
YS1306 <71446598+YS1306@users.noreply.github.com>
2025-06-18 15:34:40 +0530

    Time: 64 ms (15.81%) | Memory: 34.7 MB (19.50%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.py
+++ 131-palindrome-partitioning/palindrome-partitioning.py
@@ -1,17 +1,15 @@
 class Solution:
-    def isPali(self, st):
+    def partition(self, s: str) -> List[List[str]]:
+    
+        res = []
+
+        def isPali(st):
             n = len(st)
             for i in range(n//2):
                 if st[i] != st[n-i-1]:
                     return False
 
             return True
-
-    def partition(self, s: str) -> List[List[str]]:
-    
-        res = []
-
-        
 
         def check(nums, curr_list):
             
@@ -21,7 +19,7 @@ class Solution:
 
             for i in range(1, len(nums)+1):
                 
-                if self.isPali(nums[:i]):
+                if isPali(nums[:i]):
                     check(nums[i:], curr_list+[nums[:i]])
 
         check(s, [])
