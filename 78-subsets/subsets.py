26d0a72e316222f250c085afa3c265007363b8f6
YS1306 <71446598+YS1306@users.noreply.github.com>
2025-07-13 17:03:05 +0530

    Time: 3 ms (10.23%) | Memory: 18.1 MB (21.43%) - LeetSync

--- 78-subsets/subsets.py
+++ 78-subsets/subsets.py
@@ -2,14 +2,14 @@ class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         res = []
         n = len(nums)
-        def check(i,curr):
+        def check(nums,i,curr):
             if curr not in res:
                 res.append(curr)
             if i >=  n:
                 return
-            check(i+1, curr)
-            check(i+1, curr+[nums[i]])
+            check(nums, i+1, curr)
+            check(nums, i+1, curr+[nums[i]])
             return
         
-        check(0, [])
+        check(nums, 0, [])
         return res
