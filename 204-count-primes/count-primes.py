6195bd9df6771d67f37e08c36d15bb44aeff02b2
YS1306 <71446598+YS1306@users.noreply.github.com>
2025-07-14 17:55:22 +0530

    Time: 2741 ms (14.34%) | Memory: 75.2 MB (17.57%) - LeetSync

--- 204-count-primes/count-primes.py
+++ 204-count-primes/count-primes.py
@@ -2,16 +2,19 @@
 class Solution:
     def countPrimes(self, n: int) -> int:
         primes = [1 for i in range(n)]
-
+        res = 0
         i = 2
-        while i*i < n:
+        while i < n:
             if primes[i] == 1:
-
+                res += 1
+                if i*i >= n:
+                    i += 1
+                    continue 
                 j = i*i
                 while(j < n):
                     primes[j] = 0
                     j = j+i
             i += 1
 
-        return primes[2:].count(1)
+        return res
         
