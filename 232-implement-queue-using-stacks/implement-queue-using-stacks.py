e0534f70f483df76d7e9fde04ef850ba5180fcd4
YS1306 <71446598+YS1306@users.noreply.github.com>
2025-07-16 15:12:31 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (38.90%) - LeetSync

--- 232-implement-queue-using-stacks/implement-queue-using-stacks.py
+++ 232-implement-queue-using-stacks/implement-queue-using-stacks.py
@@ -3,48 +3,32 @@ class MyQueue:
     def __init__(self):
         self.s1 = []
         self.s2 = []
-        self.front = -1
-        self.rear = -1
 
     def push(self, x: int) -> None:
-        if self.front == self.rear == -1:
-            self.front = self.rear = 0
-            self.s1.append(x)
-        else:
-            self.rear += 1
-            self.s1.append(x)
+        self.s1.append(x)
             
 
     def pop(self) -> int:
-        print("Before",self.s1, self.front, self.rear)
-        while(self.rear - self.front > 0):
-            print(self.rear)
+        while(len(self.s1) > 1):
             self.s2.append(self.s1.pop())
-            self.rear -= 1
         temp = self.s1.pop()
-        self.front += 1
         while(len(self.s2) > 0):
             self.s1.append(self.s2.pop())
-            self.rear += 1
-        print("After",self.s1, self.front, self.rear)
         
         return temp
 
     def peek(self) -> int:
         while(len(self.s1) > 1):
             self.s2.append(self.s1.pop())
-            self.rear -= 1
 
         temp =  self.s1[-1]
 
         while len(self.s2) > 0:
             self.s1.append(self.s2.pop())
-            self.rear += 1
         return temp
         
 
     def empty(self) -> bool:
-        print(self.front, self.rear)
         return len(self.s1) == 0
         
 
