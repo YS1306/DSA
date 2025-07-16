2c3c13c26ec472e7108f113a64146ac51c8b27f2
YS1306 <71446598+YS1306@users.noreply.github.com>
2025-07-16 16:30:26 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (50.72%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -1,14 +1,13 @@
+# from collections import deque
 class Solution:
     def isValid(self, s: str) -> bool:
         stack = []
         close = {')':'(', ']':'[' , '}':'{' }
         for i in s:
-            # print(stack, i)
             if i in ['(', '[', '{']:
-                # print(i, True)
                 stack.append(i)
             else:
-                if len(stack) > 0 and stack[-1] == close[i]:
+                if len(stack) > 0 and stack[-1] == close.get(i):
                     stack.pop()
                 else:
                     return False
