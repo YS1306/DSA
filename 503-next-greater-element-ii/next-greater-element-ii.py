c8b4baba99edae89c02180433baffa8091291cad
YS1306 <71446598+YS1306@users.noreply.github.com>
2025-07-16 22:57:02 +0530

    Time: 20 ms (75.02%) | Memory: 19.3 MB (86.17%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.py
+++ 503-next-greater-element-ii/next-greater-element-ii.py
@@ -2,24 +2,21 @@ class Solution:
     def nextGreaterElements(self, nums: List[int]) -> List[int]:
         n = len(nums)
         st = []
-        # st2 = []
+
         res = [-1]*n
-        max_i = 0
+        # max_i = 0
 
-        for i in range(n):
-            if nums[i] >= nums[max_i]:
-                max_i = i
-        nums = nums[max_i+1:] + nums[:max_i+1]
-        # while(nums[-1] != max_ele):
-        #     nums = [nums[-1]] + nums[:-1]
-        #     cnt += 1
+        # for i in range(n):
+        #     if nums[i] >= nums[max_i]:
+        #         max_i = i
+        nums = nums.copy() + nums.copy()
 
-        for i in range(n-1,-1,-1):
+        for i in range(2*n-1,-1,-1):
             while st and st[-1] <= nums[i]:
                 st.pop()
-            if st:
+            if st and i < n:
                 res[i] = st[-1]
             st.append(nums[i])  
 
-        res = res[n-max_i-1:]+res[:n-max_i-1]
+        # res = res[n-max_i-1:]+res[:n-max_i-1]
         return res
