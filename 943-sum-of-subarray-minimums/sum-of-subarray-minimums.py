c8c8f6ae8b86dd738a71b63239c31e44f038a6da
YS1306 <71446598+YS1306@users.noreply.github.com>
2025-07-23 23:14:46 +0530

    Time: 131 ms (12.09%) | Memory: 22.3 MB (15.89%) - LeetSync

---
+++ 943-sum-of-subarray-minimums/sum-of-subarray-minimums.py
@@ -0,0 +1,38 @@
+class Solution:
+    def sumSubarrayMins(self, arr: List[int]) -> int:
+        n = len(arr)
+        mod = 1000000007
+        def nse(arr):
+            stack = []
+            nse_arr = [n for i in range(n)]
+            for i in range(n-1, -1, -1):
+                while(stack and arr[stack[-1]] >= arr[i]):
+                    stack.pop()
+                
+                if stack:
+                    nse_arr[i] = stack[-1]
+                stack.append(i)
+            print(nse_arr)
+            return nse_arr
+
+        def pse(arr):
+            stack = []
+            pse_arr = [-1 for i in range(n)]
+            for i in range(n):
+                while(stack and arr[stack[-1]] > arr[i]):
+                    stack.pop()
+                
+                if stack:
+                    pse_arr[i] = stack[-1]
+                stack.append(i)
+            print(pse_arr)
+            return pse_arr
+
+        nse_arr = nse(arr)
+        pse_arr = pse(arr)
+
+        res = 0
+        for i in range(n):
+            res += (nse_arr[i]-i)*(i-pse_arr[i])*arr[i]
+        print(res)            
+        return res%mod
