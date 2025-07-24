c9477988799f2f4a3f49d65f97ff7ca52a5f1778
YS1306 <71446598+YS1306@users.noreply.github.com>
2025-07-24 19:29:17 +0530

    Time: 28 ms (38.10%) | Memory: 18.5 MB (12.09%) - LeetSync

---
+++ 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.py
@@ -0,0 +1,64 @@
+class Solution:
+    def subArrayRanges(self, nums: List[int]) -> int:
+        n = len(nums)
+        def nse(nums):
+            nse_arr = [n]*n
+            stack = []
+            for i in range(n-1, -1, -1):
+                while(stack and nums[stack[-1]] > nums[i]):
+                    stack.pop()
+                if stack:
+                    nse_arr[i] = stack[-1]
+                stack.append(i)
+            print(nse_arr)
+            return nse_arr                
+
+        def pse(nums):
+            pse_arr = [-1]*n
+            stack = []
+            for i in range(n):
+                while(stack and nums[stack[-1]] >= nums[i]):
+                    stack.pop()
+                if stack:
+                    pse_arr[i] = stack[-1]
+                stack.append(i)
+            print(pse_arr)
+            return pse_arr
+
+        def nge(nums):
+            nge_arr = [n]*n
+            stack = []
+            for i in range(n-1, -1, -1):
+                while(stack and nums[stack[-1]] < nums[i]):
+                    stack.pop()
+                if stack:
+                    nge_arr[i] = stack[-1]
+                stack.append(i)
+            print(nge_arr)
+            return nge_arr                
+
+        def pge(nums):
+            pge_arr = [-1]*n
+            stack = []
+            for i in range(n):
+                while(stack and nums[stack[-1]] <= nums[i]):
+                    stack.pop()
+                if stack:
+                    pge_arr[i] = stack[-1]
+                stack.append(i)
+            print(pge_arr)
+            return pge_arr
+
+        mins = 0
+        maxs = 0
+
+        nse_arr = nse(nums)
+        pse_arr = pse(nums)
+        nge_arr = nge(nums)
+        pge_arr = pge(nums)
+
+        for i in range(len(nums)):
+            mins += (nse_arr[i]-i)*(i-pse_arr[i])*nums[i]
+            maxs += (nge_arr[i]-i)*(i-pge_arr[i])*nums[i]
+        
+        return maxs-mins
