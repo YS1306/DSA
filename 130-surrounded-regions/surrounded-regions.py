cc142eebc1b08a3e2c5018e46811d3af0d0f7a7a
YS1306 <71446598+YS1306@users.noreply.github.com>
2025-08-24 19:50:42 +0530

    Time: 4 ms (72.51%) | Memory: 22.1 MB (20.81%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.py
+++ 130-surrounded-regions/surrounded-regions.py
@@ -7,51 +7,43 @@ class Solution:
         n = len(board[0])
         visited = [[0 for j in range(n)] for i in range(m)]
         
-        region = []
 
         def dfs(i,j):
-            if i in [0,m-1] or j in [0, n-1]:
-                return False
-            v1 = True
-            v2 = True
-            v3 = True
-            v4 = True
-
+            board[i][j] = "F"
             if j < n-1 and not visited[i][j+1] and board[i][j+1] == "O":
-                region.append((i,j+1))
                 visited[i][j+1] = 1
-                v1 = dfs(i,j+1)
+                dfs(i,j+1)
             if j > 0 and not visited[i][j-1] and board[i][j-1] == "O":
-                region.append((i,j-1))
                 visited[i][j-1] = 1
-                v2 = dfs(i, j-1)
+                dfs(i, j-1)
             if i < m-1 and not visited[i+1][j] and board[i+1][j] == "O":
-                region.append((i+1,j))
                 visited[i+1][j] = 1
-                v3 = dfs(i+1,j)
+                dfs(i+1,j)
             if i > 0 and not visited[i-1][j] and board[i-1][j] == "O":
-                region.append((i-1,j))
                 visited[i-1][j] = 1
-                v4 = dfs(i-1, j)
-            
-            if v1 and v2 and v3 and v4:
-                return True
-            else:
-                return False
-                
+                dfs(i-1, j)            
 
+                
+            
+        for i in range(m):
+            if board[i][0] == "O":
+                dfs(i,0)
+            if board[i][n-1] == "O":
+                dfs(i,n-1)
+        
+        for j in range(n):
+            if board[0][j] == "O":
+                dfs(0,j)
+            if board[m-1][j] == "O":
+                dfs(m-1,j)
+                
         
         for i in range(m):
             for j in range(n):
-                if board[i][j] == "O" and not visited[i][j]:
-                    region.append((i,j))
-                    val = dfs(i,j)
-                    if val:
-                        for ind in region:
-                            r,c = ind
-                            board[r][c] = "X"
-                
-                region = []
+                if board[i][j] == "O":
+                    board[i][j] = "X"
+                elif board[i][j] == "F":
+                    board[i][j] = "O"
 
 
 
