0eb4d1ad0dacf259809c71821e901c1350e39949
YS1306 <71446598+YS1306@users.noreply.github.com>
2025-09-06 00:00:07 +0530

    Time: 49 ms (49.73%) | Memory: 23.4 MB (86.85%) - LeetSync

---
+++ 820-find-eventual-safe-states/find-eventual-safe-states.py
@@ -0,0 +1,28 @@
+from collections import deque
+class Solution:
+    def eventualSafeNodes(self, graph: List[List[int]]) -> List[int]:
+        n = len(graph)
+        inv_graph = [[] for i in range(n)]
+        in_d = [0]*n
+        q = deque()
+        res = []
+        for i in range(n):
+            for j in graph[i]:
+                in_d[i] += 1
+                inv_graph[j].append(i)
+
+        for i in range(n):
+            if in_d[i] == 0:
+                q.append(i)
+                res.append(i)
+
+        while(q):
+            curr = q.popleft()
+            for j in inv_graph[curr]:
+                in_d[j] -= 1
+                if in_d[j] == 0:
+                    q.append(j)
+                    res.append(j)
+                
+        
+        return sorted(res)
