56e5c24bb9b6bd3781ee853211f8564679345b39
YS1306 <71446598+YS1306@users.noreply.github.com>
2025-09-21 23:23:21 +0530

    Time: 374 ms (75.26%) | Memory: 19.5 MB (93.08%) - LeetSync

---
+++ 744-network-delay-time/network-delay-time.py
@@ -0,0 +1,31 @@
+from heapq import heappush, heappop 
+class Solution:
+    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+        graph = {i:[] for i in range(1,n+1)}    
+
+
+        for u,v,w in times:
+            graph[u].append((v,w))
+        
+        dist = [100000]*(n+1)
+        q = []
+
+        dist[k] = 0
+        heappush(q, (0,k))
+
+        while(q):
+            d, s = heappop(q)
+
+            for node, t in graph[s]:
+                if dist[node] > d+t:
+                    dist[node] = d+t
+                    heappush(q, (dist[node], node))
+            
+        res = 0
+
+        for k in dist[1:]:
+            if k == 100000:
+                return -1
+            if k > res:
+                res = k
+        return res
