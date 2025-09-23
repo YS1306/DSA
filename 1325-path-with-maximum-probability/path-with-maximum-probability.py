f77403d6defa5499911a8b54b4a61c5a0040a684
YS1306 <71446598+YS1306@users.noreply.github.com>
2025-09-23 23:36:48 +0530

    Time: 141 ms (22.05%) | Memory: 28.4 MB (65.68%) - LeetSync

--- 1325-path-with-maximum-probability/path-with-maximum-probability.py
+++ 1325-path-with-maximum-probability/path-with-maximum-probability.py
@@ -5,23 +5,23 @@ class Solution:
         graph = {k:[] for k in range(n)}
 
         for i in range(len(edges)):
-            graph[edges[i][0]].append((edges[i][1], (-1)*succProb[i]))
-            graph[edges[i][1]].append((edges[i][0], (-1)*succProb[i]))
+            graph[edges[i][0]].append((edges[i][1], succProb[i]))
+            graph[edges[i][1]].append((edges[i][0], succProb[i]))
 
         dist = [0]*n
         q = []
 
-        heappush(q, (1, start_node))
-        dist[start_node] = 1
+        heappush(q, (-1, start_node))
+        dist[start_node] = -1
 
         while(q):
             d, s = heappop(q)
 
             for node, prob in graph[s]:
 
-                if dist[node] > (-1)*abs(d*prob):
+                if dist[node] > d*prob:
 
-                    dist[node] = (-1)*abs(d*prob)
+                    dist[node] = d*prob
                     heappush(q, (dist[node], node))
         print(dist)
         return abs(dist[end_node])
