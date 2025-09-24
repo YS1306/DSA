c776210002698204860330a02a5774fc7054c943
YS1306 <71446598+YS1306@users.noreply.github.com>
2025-09-24 22:51:58 +0530

    Time: 531 ms (14.87%) | Memory: 19.6 MB (9.79%) - LeetSync

---
+++ 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.py
@@ -0,0 +1,34 @@
+class Solution:
+    def findTheCity(self, n: int, edges: List[List[int]], distanceThreshold: int) -> int:
+        dist = [[float('inf')]*n for i in range(n)]
+        reachable = {k:set() for k in range(n)}
+        for i in range(n):
+            dist[i][i] = 0
+        for u,v,w in edges:
+            dist[u][v] = w
+            dist[v][u] = w
+
+            if w < distanceThreshold:
+                reachable[u].add(v)
+                reachable[v].add(u)
+
+        for k in range(n):
+            for i in range(n):
+                for j in range(n):
+                    dist[i][j] = min(dist[i][j], dist[i][k]+dist[k][j])
+                    if dist[i][j] <= distanceThreshold:
+                        reachable[i].add(j)
+                        reachable[j].add(i)
+
+        min_ctr = n+1
+        city = n-1
+        print(reachable)
+
+        for i in range(n-1,-1,-1):
+            if len(reachable[i]) < min_ctr:
+                min_ctr = len(reachable[i])
+                city = i
+
+        return city
+
+
