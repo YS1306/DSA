7c119505f91638fb7b4e7e05e11554c020fd83c1
YS1306 <71446598+YS1306@users.noreply.github.com>
2025-09-26 02:33:01 +0530

    Time: 366 ms (38.47%) | Memory: 18.2 MB (85.71%) - LeetSync

--- 984-most-stones-removed-with-same-row-or-column/most-stones-removed-with-same-row-or-column.py
+++ 984-most-stones-removed-with-same-row-or-column/most-stones-removed-with-same-row-or-column.py
@@ -30,10 +30,8 @@ class Solution:
             x1, y1 = stones[i]
             for j in range(i+1,len(stones)):
                 x2, y2 = stones[j]
-                if (x1 == x2) ^ (y1 == y2):
-                    
+                if (x1 == x2) or (y1 == y2):
                     UnionBySize(i, j)
-        
 
         res = 0
         for i in range(n):
