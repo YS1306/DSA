79e59216b0b685f36c5eeba62a05ac356cd11f1c
YS1306 <71446598+YS1306@users.noreply.github.com>
2025-09-26 02:30:02 +0530

    Time: 546 ms (33.81%) | Memory: 18.5 MB (60.78%) - LeetSync

---
+++ 984-most-stones-removed-with-same-row-or-column/most-stones-removed-with-same-row-or-column.py
@@ -0,0 +1,46 @@
+class Solution:
+    def removeStones(self, stones: List[List[int]]) -> int:
+        n = len(stones)
+        parent = [i for i in range(n)]
+        size = [1]*n
+
+
+        def findP(u):
+            if parent[u] == u:
+                return u
+            parent[u] = findP(parent[u])
+            return parent[u]
+
+        def UnionBySize(u,v):
+            par_u = findP(u)
+            par_v = findP(v)
+
+            if par_u == par_v:
+                return size[par_u]
+            
+            if size[par_u] < size[par_v]:
+                parent[par_u] = par_v
+                size[par_v] += size[par_u]
+                # return size[par_v]
+            else:
+                parent[par_v] = par_u 
+                size[par_u] += size[par_v]
+                # return size[par_u]
+            
+
+        for i in range(len(stones)):
+            x1, y1 = stones[i]
+            for j in range(i+1,len(stones)):
+                x2, y2 = stones[j]
+                if (x1 == x2) ^ (y1 == y2):
+                    
+                    UnionBySize(i, j)
+        
+        print(parent)
+        print(size)
+        res = 0
+        for i in range(n):
+            if parent[i] == i:
+                res += (size[i]-1)
+
+        return res
