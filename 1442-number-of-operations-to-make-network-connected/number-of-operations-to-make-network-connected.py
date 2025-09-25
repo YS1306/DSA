96995d78c741472a75222a3cc55de87c643695e8
YS1306 <71446598+YS1306@users.noreply.github.com>
2025-09-26 01:41:15 +0530

    Time: 50 ms (30.01%) | Memory: 32.4 MB (82.32%) - LeetSync

---
+++ 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.py
@@ -0,0 +1,43 @@
+class Solution:
+    def makeConnected(self, n: int, connections: List[List[int]]) -> int:
+        
+        parent = [i for i in range(n)]
+        size = [1]*n
+        res = 0
+
+        def find(u):
+            if parent[u] == u:
+                return u
+            parent[u] = find(parent[u])
+            return parent[u]
+
+        def UnionBySize(res, u,v):
+            par_u = find(u)
+            par_v = find(v)
+
+            if par_u == par_v:
+                res += 1
+            elif size[par_u] < size[par_v]:
+                parent[par_u] = par_v
+                size[par_v] += size[par_u]
+            else:
+                parent[par_v] = par_u
+                size[par_u] += size[par_v]
+
+            # print(par_u, par_v)
+            
+            return res
+            
+        for u, v in connections:
+            res = UnionBySize(res, u,v)
+
+        for node in range(n):
+            find(node)
+
+        comp = len(set(parent))
+        
+        if res >= comp-1:
+            return comp-1
+        return -1
+
+            
