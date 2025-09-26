c856a4d5a47b7c14dc1faabb3d2bbab3836b0bbb
YS1306 <71446598+YS1306@users.noreply.github.com>
2025-09-26 23:28:53 +0530

    Time: 659 ms (84.98%) | Memory: 73.6 MB (6.89%) - LeetSync

--- 854-making-a-large-island/making-a-large-island.py
+++ 854-making-a-large-island/making-a-large-island.py
@@ -1,10 +1,6 @@
 import copy
 class Solution:
     def largestIsland(self, grid: List[List[int]]) -> int:
-        # grid = [[0 for j in range(m)] for i in range(n)]
-        
-        
-        
         ctr = -1
         belongsTo = defaultdict(int)
         n = len(grid)
@@ -41,66 +37,9 @@ class Solution:
         parent = [i for i in range(par+1)]
         size.append(0)
         largest = max(size)
-        
-        # print(parent)
-        # print(size)
-
-        def UnionBySize(parent, size, u, v):
-            # def findP(u):
-            #     if parent[u] == u:
-            #         return u
-            #     parent[u] = findP(parent[u])
-            #     return parent[u]
-            # print(u, v)
-            par_u = parent[u];   par_v = parent[v]
-            if par_u == par_v:
-                # return par_v
-                return parent, size, par_u, size[par_u]
-            
-        
-            parent[par_v] = par_u
-            size[par_u] += size[par_v];     size[par_v] = 0
-            return parent, size, par_u, size[par_u]
-            
-
-        def process(parent, size, belongsTo, i, j):
-            curr_large = 1
-            belongsTo[(i, j)] = par
-            if j < n-1 and grid[i][j+1]:
-                parent, size, new, upd = UnionBySize(parent, size, belongsTo[(i,j+1)], belongsTo[(i,j)])        
-                if upd > curr_large:
-                    curr_large = upd
-                belongsTo[(i,j)] = new
-                # visited[i][j] = True
-                # dfs(par, i, j+1)
-            if i < n-1 and grid[i+1][j]:
-                parent, size, new, upd = UnionBySize(parent, size, belongsTo[(i+1,j)], belongsTo[(i,j)])
-                if upd > curr_large:
-                    curr_large = upd
-                belongsTo[(i, j)] = new
-                # belongsTo[(i,j)] = new
-                # visited[i][j] = True
-                # dfs(par, i+1, j)
-            if j > 0 and grid[i][j-1]:
-                parent, size, new, upd = UnionBySize(parent, size, belongsTo[(i,j-1)], belongsTo[(i,j)])
-                if upd > curr_large:
-                    curr_large = upd
-                belongsTo[(i,j)] = new
-                # visited[i][j] = True
-                # dfs(par, i, j-1)
-            if i > 0 and grid[i-1][j]:
-                parent, size, new, upd = UnionBySize(parent, size, belongsTo[(i-1, j)], belongsTo[(i,j)])
-                if upd > curr_large:
-                    curr_large = upd
-                belongsTo[(i,j)] = new
-                # visited[i][j] = True
-                # dfs(par, i-1, j)
-            return curr_large
 
         ctr = par
 
-        # parent_copy = copy.deepcopy(parent)
-        # size_copy = copy.deepcopy(size)
         for i, j in cands:
             curr = 1
             neighbours = set()
@@ -108,34 +47,19 @@ class Solution:
                 neighbours.add(belongsTo[(i,j+1)])
 
             if i < n-1 and grid[i+1][j]:
-                # curr += size[]
                 neighbours.add(belongsTo[(i+1, j)])
 
             if j > 0 and grid[i][j-1]:
-                # curr += size[]
                 neighbours.add(belongsTo[(i,j-1)])
 
             if i > 0 and grid[i-1][j]:
-                # curr += size[]
                 neighbours.add(belongsTo[(i-1,j)])
             
             for island in neighbours:
                 curr += size[island]
-                
+
             if curr > largest:
                 largest = curr
 
-            # grid[i][j] = 1
-            # belongsTo[(i,j)] = ctr
-            # parent[ctr] = ctr
-            # size[ctr] = 1
-            
-            # curr = process(parent.copy(), size.copy(), belongsTo, i, j)
-
-
-            # if curr > largest:
-            #     largest = curr
-
-            # grid[i][j] = 0
 
         return largest
