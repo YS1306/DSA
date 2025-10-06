dccbea41fe38fd8d0cccb032d4a5a848bb144d44
YS1306 <71446598+YS1306@users.noreply.github.com>
2025-10-07 02:27:08 +0530

    Time: 3 ms (31.81%) | Memory: 16.4 MB (40.80%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -7,14 +7,17 @@ public:
         // Store minimum sum between right and down
 
         dp[m-1][n-1] = grid[m-1][n-1];
-
-        for(int i = m-2; i >= 0; i--){
-            dp[i][n-1] = grid[i][n-1] + dp[i+1][n-1];
+        int big  = max(m,n);
+        for(int i = big-2; i >= 0; i--){
+            if(i < m-1)
+                dp[i][n-1] = grid[i][n-1] + dp[i+1][n-1];
+            if(i < n-1)
+                dp[m-1][i] = grid[m-1][i] + dp[m-1][i+1];
         }
 
-        for(int i = n-2; i >= 0; i--){
-            dp[m-1][i] = grid[m-1][i] + dp[m-1][i+1];
-        }
+        // for(int i = n-2; i >= 0; i--){
+            
+        // }
 
         for(int i=m-2; i>=0; i--){
             for(int j = n-2; j>=0; j--){
