5c933ddb2d97a959d0972b92744d0f315a44663f
YS1306 <71446598+YS1306@users.noreply.github.com>
2025-10-07 01:44:51 +0530

    Time: 1 ms (24.58%) | Memory: 9.6 MB (9.52%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -17,12 +17,7 @@ public:
             }
         }
         
-        for(int i=0; i<m; i++){
-            for(int j = 0; j< n; j++){
-                cout<<dp[i][j]<<"\t";
-            }
-            cout<<endl;
-        }
+        
         return dp[0][0];
         
     }
