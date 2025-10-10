e90f688d9a69841d38b3bd61b077daeb5b4faae9
YS1306 <71446598+YS1306@users.noreply.github.com>
2025-10-11 00:52:43 +0530

    Time: 163 ms (27.77%) | Memory: 17.3 MB (67.49%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,21 +1,13 @@
 #include<numeric>
 class Solution {
 public:
-
-    bool func(int ind,int s1,int s2,vector<int> nums, vector<vector<vector<int>>>& dp){
-        if (dp[ind][s1][s2] != -1)
-            return dp[ind][s1][s2];
-        if(ind == 0)
-            return (min(s1,s2)+nums[0] == max(s1, s2));
-        bool take_in_1 = func(ind-1, s1+nums[ind], s2, nums, dp);
-        bool take_in_2 = func(ind-1, s1, s2+nums[ind], nums, dp);
-        dp[ind][s1][s2] = (take_in_1 || take_in_2);
-        return dp[ind][s1][s2];
-    }
-
     bool canPartition(vector<int>& nums) {
         int n = nums.size();
-        int total = accumulate(nums.begin(), nums.end(), 0);
+        int total = 0;
+        for(auto num:nums){
+            total += num;
+        }
+        // int total = accumulate(nums.begin(), nums.end(), 0);
         if(total%2) 
             return false;
         int target = int(total/2);
