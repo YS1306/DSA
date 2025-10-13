64dda23cf38d478c5b156d5cbe91c661be562b1d
YS1306 <71446598+YS1306@users.noreply.github.com>
2025-10-14 00:24:07 +0530

    Time: 83 ms (7.50%) | Memory: 49 MB (37.96%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -11,16 +11,19 @@ public:
             if(coins[i] <= amount)
                 dp[i][coins[i]] = 1;
         }
-        for(int j = 0; j<=amount; j+=coins[0])
-            dp[0][j] = int(j/coins[0]);
+        // for(int j = 0; j<=amount; j+=coins[0])
+        //     dp[0][j] = int(j/coins[0]);
         
-        for(int i=1; i< n; i++){
+        for(int i=0; i< n; i++){
             for(int j=1; j<= amount; j++){
-                not_take = dp[i-1][j];
+                not_take = 100000;
+                if(i > 0)
+                    not_take = dp[i-1][j];
                 repeat = 100000;
                 no_repeat = 100000;
                 if(j >= coins[i]){
                     repeat = 1+dp[i][j-coins[i]];
+                    if(i > 0)
                     no_repeat = 1+dp[i-1][j-coins[i]];
                 }
                 if( not_take <= repeat && not_take <= no_repeat)
