8e270b850fbc3d9a972e55a60808406db947aa02
YS1306 <71446598+YS1306@users.noreply.github.com>
2025-10-14 00:20:30 +0530

    Time: 48 ms (30.18%) | Memory: 48.9 MB (42.10%) - LeetSync

---
+++ 322-coin-change/coin-change.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        int n = coins.size();
+        vector<vector<int>> dp(n, vector<int>(amount+1, 100000));
+        for(int i = 0; i<n; i++){
+            dp[i][0] = 0;
+            if(coins[i] <= amount)
+                dp[i][coins[i]] = 1;
+        }
+        for(int j = 0; j<=amount; j+=coins[0])
+            dp[0][j] = int(j/coins[0]);
+        
+        for(int i=1; i< n; i++){
+            for(int j=1; j<= amount; j++){
+                int not_take = dp[i-1][j];
+                int repeat = 100000;
+                int no_repeat = 100000;
+                if(j >= coins[i]){
+                    repeat = 1+dp[i][j-coins[i]];
+                    no_repeat = 1+dp[i-1][j-coins[i]];
+                }
+                if( not_take <= repeat && not_take <= no_repeat)
+                    dp[i][j] = not_take;
+                else if(no_repeat <= repeat && no_repeat <= not_take )
+                    dp[i][j] = no_repeat;
+                else
+                    dp[i][j] = repeat;
+            }
+        }
+
+        if(dp[n-1][amount] >= 100000)
+            return -1;
+        return dp[n-1][amount];
+    }
+};
