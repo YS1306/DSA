5b992eaa88090fd5147f3ca732ca1cb660426664
YS1306 <71446598+YS1306@users.noreply.github.com>
2025-10-14 23:33:30 +0530

    Time: 519 ms (21.94%) | Memory: 188.7 MB (5.08%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -12,18 +12,12 @@ public:
         if(target > total || target < -1*total)
             return 0;
         
-        // if(target == total and target == -1*total)
-        //     return 2;
-        // cout<<target<<"\t"<<total;
         if(target == 0 && total == 0){
             return 1<<n;
         }
 
         vector<unordered_map<int, int>> dp(n);
         for(int i = 0; i<n; i++){
-            // for(int j=-1*total; j<=target; j++){
-            //     dp[i][j] = 0;
-            // }
             dp[i][nums[i]] += 1;
             dp[i][(-1)*nums[i]] += 1;
         }
@@ -31,9 +25,11 @@ public:
         
         for(int i = 1; i<n; i++){
             for(int j = -1*total+1; j<=total ; j++){
-                int plus = dp[i-1][j-nums[i]];
-                int minus = dp[i-1][j+nums[i]];
-
+                int plus =0, minus = 0;
+                if(j-nums[i] <= total && j-nums[i] >= -1*total){
+                    plus = dp[i-1][j-nums[i]];
+                    minus = dp[i-1][j+nums[i]];
+                }
                 dp[i][j] = plus+minus; 
             }
         } 
