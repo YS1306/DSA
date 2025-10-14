f91a71a33694ac366235486bfb9f0a85058f2c5e
YS1306 <71446598+YS1306@users.noreply.github.com>
2025-10-15 00:23:31 +0530

    Time: 4 ms (79.83%) | Memory: 15.5 MB (48.78%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -2,39 +2,45 @@
 class Solution {
 public:
     int findTargetSumWays(vector<int>& nums, int target) {
+    
         int n = nums.size();
         int total = 0;
         for(auto it:nums)
-            total += abs(it);
-        total = abs(total);
-        if(target < 0)
-            target *= -1;
-        if(target > total || target < -1*total)
+            total += it;
+        // if(target < 0)
+        //     target *= -1;
+        if(abs(target) > total || (total+target)%2 != 0)
             return 0;
-        
         if(target == 0 && total == 0){
             return 1<<n;
         }
-
-        vector<unordered_map<int, int>> dp(n);
-        for(int i = 0; i<n; i++){
-            dp[i][nums[i]] += 1;
-            dp[i][(-1)*nums[i]] += 1;
-        }
-
         
-        for(int i = 1; i<n; i++){
-            for(int j = -1*total+1; j<=total ; j++){
+
+        int D = (total+target)/2;
+
+        vector<vector<int>> dp(n+1, vector<int>(D+1, 0));
+        
+        dp[0][0] = 1;
+        // if(nums[0] < D){
+        //     dp[0][nums[0]] = 1;
+        //     if(nums[0] == 0)
+        //         dp[0][0] = 2;
+        // }
+        
+        for(int i = 1; i<n+1; i++){
+            for(int j = 0; j<=D ; j++){
                 int plus =0, minus = 0;
-                if(j-nums[i] <= total && j-nums[i] >= -1*total){
-                    plus = dp[i-1][j-nums[i]];
-                    minus = dp[i-1][j+nums[i]];
+                if(nums[i-1] <= j){
+                    plus = dp[i-1][j-nums[i-1]];
+                    
+                    // minus = dp[i-1][j+nums[i]];
                 }
+                minus = dp[i-1][j];
                 dp[i][j] = plus+minus; 
             }
         } 
     
-        return dp[n-1][target];
+        return dp[n][D];
         
     }
 };
