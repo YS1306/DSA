f270b1cb2230b422c816736c6ce906faa3579d7d
YS1306 <71446598+YS1306@users.noreply.github.com>
2025-10-14 23:20:20 +0530

    Time: 563 ms (21.70%) | Memory: 194.2 MB (5.08%) - LeetSync

---
+++ 494-target-sum/target-sum.cpp
@@ -0,0 +1,44 @@
+#include<numeric>
+class Solution {
+public:
+    int findTargetSumWays(vector<int>& nums, int target) {
+        int n = nums.size();
+        int total = 0;
+        for(auto it:nums)
+            total += abs(it);
+        
+        if(target < 0)
+            target *= -1;
+        if(target > abs(total) || target < -1*abs(total))
+            return 0;
+        
+        // if(target == abs(total) and target == -1*abs(total))
+        //     return 2;
+        // cout<<target<<"\t"<<total;
+        if(target == 0 && total == 0){
+            return pow(2, n);
+        }
+
+        vector<unordered_map<int, int>> dp(n);
+        for(int i = 0; i<n; i++){
+            for(int j=-1*abs(total); j<abs(total)+1; j++){
+                dp[i][j] = 0;
+            }
+            dp[i][nums[i]] += 1;
+            dp[i][(-1)*nums[i]] += 1;
+        }
+
+        
+        for(int i = 1; i<n; i++){
+            for(int j = -1*abs(total)+1; j<=abs(total) ; j++){
+                int plus = dp[i-1][j-nums[i]];
+                int minus = dp[i-1][j+nums[i]];
+
+                dp[i][j] = plus+minus; 
+            }
+        } 
+    
+        return dp[n-1][target];
+        
+    }
+};
