802c4fa8cbafa618420eba3965df3d42da57b2c7
YS1306 <71446598+YS1306@users.noreply.github.com>
2025-10-15 00:31:37 +0530

    Time: 3 ms (87.95%) | Memory: 15.4 MB (48.78%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -9,38 +9,37 @@ public:
             total += it;
         // if(target < 0)
         //     target *= -1;
-        if(abs(target) > total || (total+target)%2 != 0)
+        if(target > total || (total-target)%2 != 0)
             return 0;
         if(target == 0 && total == 0){
             return 1<<n;
         }
         
 
-        int D = (total+target)/2;
+        int D = (total-target)/2;
 
-        vector<vector<int>> dp(n+1, vector<int>(D+1, 0));
+        vector<vector<int>> dp(n, vector<int>(D+1, 0));
         
         dp[0][0] = 1;
-        // if(nums[0] < D){
-        //     dp[0][nums[0]] = 1;
-        //     if(nums[0] == 0)
-        //         dp[0][0] = 2;
-        // }
+        if(nums[0] <= D){
+            dp[0][nums[0]] = 1;
+            if(nums[0] == 0)
+                dp[0][0] = 2;
+        }
         
-        for(int i = 1; i<n+1; i++){
+        for(int i = 1; i<n; i++){
             for(int j = 0; j<=D ; j++){
                 int plus =0, minus = 0;
-                if(nums[i-1] <= j){
-                    plus = dp[i-1][j-nums[i-1]];
+                if(nums[i] <= j){
+                    plus = dp[i-1][j-nums[i]];
                     
-                    // minus = dp[i-1][j+nums[i]];
                 }
                 minus = dp[i-1][j];
                 dp[i][j] = plus+minus; 
             }
         } 
     
-        return dp[n][D];
+        return dp[n-1][D];
         
     }
 };
