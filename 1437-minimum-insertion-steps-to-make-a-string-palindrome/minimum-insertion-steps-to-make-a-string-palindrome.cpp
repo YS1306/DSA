19e15ebfcb9ec44ad7c311b2485d680a6bf0843a
YS1306 <71446598+YS1306@users.noreply.github.com>
2025-10-16 00:12:25 +0530

    Time: 35 ms (50.14%) | Memory: 31.6 MB (39.02%) - LeetSync

--- 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -8,13 +8,15 @@ public:
                 if(s[i-1] == s[n-j])
                     dp[i][j] = 1+dp[i-1][j-1];
                 else{
-                    if(dp[i-1][j] > dp[i][j-1])
-                        dp[i][j] = dp[i-1][j];
-                    else
-                        dp[i][j] = dp[i][j-1];
+                    // if(dp[i-1][j] > dp[i][j-1])
+                    //     dp[i][j] = dp[i-1][j];
+                    // else
+                    //     dp[i][j] = dp[i][j-1];
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
                 }
                 
             }
+
         }        
         
         return n-dp[n][n];
