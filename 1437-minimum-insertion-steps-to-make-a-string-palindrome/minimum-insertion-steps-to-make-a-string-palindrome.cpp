a99a11204cbe801fdc76ca219fd6fae3297ce459
YS1306 <71446598+YS1306@users.noreply.github.com>
2025-10-16 00:11:10 +0530

    Time: 37 ms (30.17%) | Memory: 31.4 MB (60.42%) - LeetSync

--- 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -3,7 +3,6 @@ public:
     int minInsertions(string s) {
         int n = s.length();
         vector<vector<int>> dp(n+1, vector<int>(n+1, 0));
-        int res = 0;
         for(int i = 1; i <= n; ++i){
             for(int j = 1; j <= n; ++j){
                 if(s[i-1] == s[n-j])
@@ -14,11 +13,10 @@ public:
                     else
                         dp[i][j] = dp[i][j-1];
                 }
-                if(i == j)
-                    res+= (dp[i][j] == dp[i-1][j-1]);
+                
             }
         }        
         
-        return res;
+        return n-dp[n][n];
     }
 };
