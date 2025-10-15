73c3c66767ac91eb19a350958eaaac4f61108793
YS1306 <71446598+YS1306@users.noreply.github.com>
2025-10-15 23:31:16 +0530

    Time: 98 ms (11.38%) | Memory: 76 MB (21.77%) - LeetSync

--- 516-longest-palindromic-subsequence/longest-palindromic-subsequence.cpp
+++ 516-longest-palindromic-subsequence/longest-palindromic-subsequence.cpp
@@ -6,7 +6,7 @@ public:
         int n = s.length();
         vector<vector<int>> dp(n+1, vector<int>(n+1, 0));
 
-        for(int i = 1; i<=n; ++i){
+        for(int i = 1; i <= n; ++i){
             for(int j = 1; j <= n; ++j){
                 if(s[i-1] == s2[j-1])
                     dp[i][j] = 1+dp[i-1][j-1];
