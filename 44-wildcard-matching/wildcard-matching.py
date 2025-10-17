2c14416f5ed89514e5e7a666ae7574cda57c232c
YS1306 <71446598+YS1306@users.noreply.github.com>
2025-10-18 01:05:34 +0530

    Time: 950 ms (6.17%) | Memory: 49.2 MB (28.15%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.py
+++ 44-wildcard-matching/wildcard-matching.py
@@ -6,16 +6,8 @@ class Solution:
         dp = [[False]*(m+1) for i in range(n+1)]
 
         dp[0][0] = True
-        # if m == 0:
-        #     if n == 0:
-        #         return True
-        #     else:
-        #         return False
-        
-        # if p[0] == "*":
-        #     for i in range(n+1):
-        #         dp[i][0] = True
-        #         dp[i][1] = True
+           
+    
         for j in range(1,m+1):
             if(p[j-1] == "*"):
                 dp[0][j] = dp[0][j-1]
@@ -24,15 +16,12 @@ class Solution:
 
         for i in range(1,n+1):
             for j in range(1, m+1):
-                if s[i-1] == p[j-1]:
+                if s[i-1] == p[j-1] or p[j-1] == "?":
                     dp[i][j] = dp[i-1][j-1]
+                elif p[j-1] == "*":
+                    dp[i][j] = dp[i][j-1] or dp[i-1][j]
                 else:
-                    if p[j-1] == '?':
-                        dp[i][j] = dp[i-1][j-1]
-                    elif p[j-1] == "*":
-                        dp[i][j] = dp[i][j-1] or dp[i-1][j]
-                    else:
-                        dp[i][j] = False
+                    dp[i][j] = False
         
 
         # print(dp)
