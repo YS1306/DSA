1932ab7a0de348a66996de6e82e9ea8062204da8
YS1306 <71446598+YS1306@users.noreply.github.com>
2025-10-19 15:13:44 +0530

    Time: 12 ms (8.18%) | Memory: 21.2 MB (8.41%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
@@ -7,7 +7,14 @@ class Solution:
         dp[n][1] = 0
 
         for i in range(n-1, -1, -1):
-            dp[i][0] = max(dp[i+1][0], -prices[i]+dp[i+1][1])
-            dp[i][1] = max(dp[i+1][1], prices[i]+dp[i+1][0])
+            if dp[i+1][0] > -prices[i]+dp[i+1][1]:
+                dp[i][0] = dp[i+1][0]
+            else:
+                dp[i][0] = -prices[i]+dp[i+1][1]
+            
+            if dp[i+1][1] > prices[i]+dp[i+1][0]:
+                dp[i][1] = dp[i+1][1]
+            else:
+                dp[i][1] = prices[i]+dp[i+1][0]
 
         return dp[i][0]  
