807a93ecca3d45532bff5c615d8648bdd72d6949
YS1306 <71446598+YS1306@users.noreply.github.com>
2025-10-19 14:47:17 +0530

    Time: 0 ms (100.00%) | Memory: 18.7 MB (70.14%) - LeetSync

---
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
@@ -0,0 +1,17 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        # Can be solved using the nge approach
+        n = len(prices)
+        i = 0
+        j = 1
+        profit = 0
+        while(j < n):
+            if prices[i] <= prices[j]:
+                profit += prices[j]-prices[i]
+                i = j
+                j = i+1
+            else:
+                i = j
+                j = i+1
+        print(profit)
+        return profit
