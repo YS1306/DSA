33841a803f9f985d4d5eb319bfa3a8b2396b68e7
YS1306 <71446598+YS1306@users.noreply.github.com>
2025-10-23 19:20:33 +0530

    Time: 1161 ms (24.78%) | Memory: 60.8 MB (26.62%) - LeetSync

---
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.py
@@ -0,0 +1,14 @@
+from heapq import heapify, heappush, heappop
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:  
+        n = len(prices)
+        dp = [[[0]*3 for j in range(2)] for i in range(n+1) ]
+
+        for i in range(n-1, -1, -1):
+            for buy in range(2):
+                for cap in range(1,3):
+                    if buy:
+                        dp[i][buy][cap] = max(-prices[i]+dp[i+1][0][cap], dp[i+1][1][cap])
+                    else:
+                        dp[i][buy][cap] = max(prices[i]+dp[i+1][1][cap-1], dp[i+1][0][cap])
+        return dp[0][1][2]
