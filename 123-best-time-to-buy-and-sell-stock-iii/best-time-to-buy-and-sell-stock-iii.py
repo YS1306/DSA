8a2b2c4e83aeb010b95122df723e8b35c340e78e
YS1306 <71446598+YS1306@users.noreply.github.com>
2025-10-23 19:22:54 +0530

    Time: 975 ms (27.75%) | Memory: 60.9 MB (24.51%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.py
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.py
@@ -8,7 +8,15 @@ class Solution:
             for buy in range(2):
                 for cap in range(1,3):
                     if buy:
-                        dp[i][buy][cap] = max(-prices[i]+dp[i+1][0][cap], dp[i+1][1][cap])
+                        if -prices[i]+dp[i+1][0][cap] > dp[i+1][1][cap]:
+                            dp[i][buy][cap] = -prices[i]+dp[i+1][0][cap]
+                        else:
+                            dp[i][buy][cap] = dp[i+1][1][cap]
+                        # dp[i][buy][cap] = max(-prices[i]+dp[i+1][0][cap], dp[i+1][1][cap])
                     else:
-                        dp[i][buy][cap] = max(prices[i]+dp[i+1][1][cap-1], dp[i+1][0][cap])
+                        if prices[i]+dp[i+1][1][cap-1] > dp[i+1][0][cap]:
+                            dp[i][buy][cap] = prices[i]+dp[i+1][1][cap-1]
+                        else:
+                            dp[i][buy][cap] = dp[i+1][0][cap]
+                        # dp[i][buy][cap] = max(prices[i]+dp[i+1][1][cap-1], dp[i+1][0][cap])
         return dp[0][1][2]
